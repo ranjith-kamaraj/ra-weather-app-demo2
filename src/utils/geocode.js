20390da8405688c85fb7630d384e7b8d220fc11a
Ranjith Kamaraj <42859314+ranjith18996@users.noreply.github.com>
2021-11-22 18:49:34 +0530

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,7 +4,7 @@ const { get } = require('lodash');
 
 const { MAPBOX_BASE_URL, MAPBOX_ACCESS_KEY } = process.env;
 
-console.log(MAPBOX_BASE_URL);
+console.log("Map Box URL" + MAPBOX_BASE_URL);
 
 const geocode = (address, callback) =>{
     let mapBoxUrl = `${MAPBOX_BASE_URL}/geocoding/v5/mapbox.places/${address}.json?limit=1&access_token=${MAPBOX_ACCESS_KEY}`;
